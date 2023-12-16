8ffed989c1a7c4c5a49b33dd3cc8df2f267ffedd
unknown <ritikakes33@gmail.com>
2023-12-16 16:59:35 +0530

    practice

---
+++ L21-ArrayQuestions/arraySum.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> reverse(vector<int> ans)
+{
+    int s = 0;
+    int e = ans.size() - 1;
+    while (s < e)
+    {
+        swap(ans[s], ans[e]);
+        s++;
+        e--;
+    }
+    return ans;
+}
+vector<int> findArraySum(vector<int> &a, int n, vector<int> &b, int m)
+{
+    int i = n - 1;
+    int j = m - 1;
+    int carry = 0;
+
+    vector<int> ans;
+    // case 1
+    while (i >= 0 && j >= 0)
+    {
+        int val1 = a[i];
+        int val2 = b[j];
+        int sum = val1 + val2 + carry;
+        carry = sum / 10;
+        sum = sum % 10;
+        ans.push_back(sum);
+        i--;
+        j--;
+    }
+    // case 2
+    while (i >= 0)
+    {
+        int sum = a[i] + carry;
+        carry = sum / 10;
+        sum = sum % 10;
+        ans.push_back(sum);
+        i--;
+    }
+    // case3
+    while (j >= 0)
+    {
+        int sum = b[j] + carry;
+        carry = sum / 10;
+        sum = sum % 10;
+        ans.push_back(sum);
+        j--;
+    }
+    while (carry != 0)
+    {
+        int sum = carry;
+        carry = sum / 10;
+        sum = sum % 10;
+        ans.push_back(sum);
+    }
+    return reverse(ans);
+}
