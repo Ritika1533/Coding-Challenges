93609ce4a64cd78d333a089408a5ff70ea91c0dc
unknown <ritikakes33@gmail.com>
2024-05-09 21:20:23 +0530

    learning

---
+++ BinarySearchLUV/50binarySearch.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+void arrayInput(int *arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+}
+int binarySearch(int *arr, int n, int key)
+{
+    int s = 0;
+    int e = n - 1;
+    while (e - s > 1)
+    {
+        int mid = s + e / 2;
+        if (arr[mid] < key)
+        {
+            s = mid + 1;
+        }
+        else
+        {
+            e = mid;
+        }
+    }
+    if (arr[s] == key)
+    {
+        return s;
+    }
+    else if (arr[e] == key)
+    {
+        return e;
+    }
+    else
+    {
+        return -1;
+    }
+}
+int main()
+{
+    int arr[10];
+    int n, key;
+    cout << "enter the no. of terms of array" << endl;
+    cin >> n;
+    cout << "enter the array elements" << endl;
+    arrayInput(arr, n);
+    cout << "enter the element you want to search" << endl;
+    cin >> key;
+    int ans = binarySearch(arr, n, key);
+    if (ans != -1)
+    {
+        cout << "found at " << ans + 1 << endl;
+    }
+    else
+    {
+        cout << " not found" << endl;
+    }
+}
