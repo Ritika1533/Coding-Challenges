addb5f351c0cbbbb8deb3b0def55200068a4b8c5
unknown <ritikakes33@gmail.com>
2024-06-15 22:16:34 +0530

    dsa

--- 44LinkedListSTRIVER/Linked List/l1.cpp
+++ 44LinkedListSTRIVER/Linked List/l1.cpp
@@ -12,10 +12,47 @@ public:
         this->next = NULL;
     }
 };
+// insert at head
+void insertAtHead(Node *&head, int data)
+{
+    Node *temp = new Node(data);
+    temp->next = head;
+    head = temp;
+}
+// print
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+void insertAtTail(Node *&head, int data)
+{
+    Node *temp = new Node(data);
+    Node *tail = head;
+    while (tail->next != NULL)
+    {
+        tail = tail->next;
+    }
+    tail->next = temp;
+}
+// main function
 int main()
 {
+    // first node
     Node *node1 = new Node(20);
-    cout << node1->data << endl;
-    cout << node1->next;
+    Node *head = node1;
+    Node *tail = node1;
+    // insertAtHead(head, 15);
+    // insertAtHead(head, 10);
+    // insertAtHead(head, 5);
+    insertAtTail(tail, 25);
+    insertAtTail(tail, 30);
+    insertAtTail(tail, 35);
+    print(head);
     return 0;
 }
